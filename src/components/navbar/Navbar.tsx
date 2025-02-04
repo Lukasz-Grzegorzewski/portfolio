f6a218d12fe015cae6fa58738bbf46a547004d62
lukasz <grzegorzewski.luk@gmail.com>
2025-02-04 18:31:56 +0100

    add Mobile Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import { useTranslation } from "react-i18next";
 import { SetActiveSectionType } from "@pages/index";
 import { useLocaleLanguageContext } from "@src/contexts/LocaleLanguageContext";
 import { LocalLanguage } from "@src/types/localLanguage.type";
+import NavbarDesktop from "./desktop/NavbarDesktop";
+import NavbarMobile from "./desktop/NavbarMobile";
 
 type NavbarProps = SetActiveSectionType & {
   activeSection: string;
@@ -15,69 +17,24 @@ const Navbar = ({
   setActiveSection,
   setIsNavClick,
 }: NavbarProps) => {
-  const { t } = useTranslation("common");
-  const { localeLanguage, changLanguage } = useLocaleLanguageContext();
-
-  // const buttons = ["Home", "Projects", "Contact"];
-  const buttons = [
-    { label: t("navbar.home"), href: "Home" },
-    { label: t("navbar.projects"), href: "Projects" },
-    { label: t("navbar.contact"), href: "Contact" },
-  ];
-
-  const handleNavClick = (section: string) => {
-    setIsNavClick(true);
-    setActiveSection(section);
-    setTimeout(() => setIsNavClick(false), 1000);
-  };
-
   return (
-    <nav className="fixed z-10 w-full max-w-screen-lg justify-end p-5 hidden sm:flex">
-      <ul className="flex gap-10 justify-end ">
-        {buttons.map((section) => (
-          <Link key={section.href} to={`#${section.href}`}>
-            <li>
-              <button
-                type="button"
-                onClick={() => handleNavClick(section.href)}
-                className={`${activeSection === section.href ? "text-primary underline underline-offset-4" : "text-secondary"} transition-all duration-500`}
-              >
-                {section.label}
-              </button>
-            </li>
-          </Link>
-        ))}
+    <>
+      <div className="hidden sm:flex">
+        <NavbarDesktop
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+          setIsNavClick={setIsNavClick}
+        />
+      </div>
 
-        <li>
-          <a
-            href={`${process.env.DOMAIN}/cv/CV_Lukasz_Grzegorzewski_${localeLanguage}.pdf`}
-            target="_blank"
-            rel="noreferrer"
-            className="text-secondary"
-          >
-            CV
-          </a>
-        </li>
-
-        <li>
-          <select
-            value={localeLanguage}
-            onChange={(e) => changLanguage(e.target.value as LocalLanguage)}
-            className="bg-transparent border-none ml-2 text-secondary focus:outline-none flex gap-0"
-          >
-            <option value="fr">
-              <span className="text-3xl">🇫🇷</span> Français
-            </option>
-            <option value="en">
-              <span className="text-3xl">🇬🇧</span> English
-            </option>
-            <option value="pl">
-              <span className="text-3xl">🇵🇱</span> Polski
-            </option>
-          </select>
-        </li>
-      </ul>
-    </nav>
+      <div className="sm:hidden">
+        <NavbarMobile
+          activeSection={activeSection}
+          setActiveSection={setActiveSection}
+          setIsNavClick={setIsNavClick}
+        />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/navbar/desktop/NavbarMobile.tsx
@@ -0,0 +1,104 @@
+import { Link, navigate } from "gatsby";
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { SetActiveSectionType } from "@pages/index";
+import { useLocaleLanguageContext } from "@src/contexts/LocaleLanguageContext";
+import { LocalLanguage } from "@src/types/localLanguage.type";
+
+type NavbarMobileProps = SetActiveSectionType & {
+  activeSection: string;
+  setIsNavClick: (isNavClick: boolean) => void;
+};
+
+const NavbarMobile = ({
+  activeSection,
+  setActiveSection,
+  setIsNavClick,
+}: NavbarMobileProps) => {
+  const { t } = useTranslation("common");
+  const { localeLanguage, changLanguage } = useLocaleLanguageContext();
+
+  const buttons = [
+    {
+      label: t("navbar.home"),
+      href: "Home",
+      iconUrlDark: "/images/icons/home_dark.png",
+      iconUrlBlue: "/images/icons/home_blue.png",
+    },
+    {
+      label: t("navbar.projects"),
+      href: "Projects",
+      iconUrlDark: "/images/icons/projects_dark.png",
+      iconUrlBlue: "/images/icons/projects_blue.png",
+    },
+    {
+      label: t("navbar.contact"),
+      href: "Contact",
+      iconUrlDark: "/images/icons/contact_dark.png",
+      iconUrlBlue: "/images/icons/contact_blue.png",
+    },
+  ];
+
+  const handleNavClick = (section: string) => {
+    setIsNavClick(true);
+    setActiveSection(section);
+    setTimeout(() => setIsNavClick(false), 1000);
+  };
+
+  return (
+    <nav className="fixed bottom-3 right-3 left-3 z-10 bg-transparent rounded-full outline-1 outline outline-secondary-dark backdrop-blur-md">
+      <ul className="flex justify-around items-center p-3 gap-5 ">
+        {buttons.map((section) => (
+          <Link key={section.href} to={`#${section.href}`}>
+            <li>
+              <button
+                type="button"
+                onClick={() => handleNavClick(section.href)}
+              >
+                <img
+                  src={
+                    activeSection === section.href
+                      ? section.iconUrlBlue
+                      : section.iconUrlDark
+                  }
+                  alt={section.label}
+                />
+              </button>
+            </li>
+          </Link>
+        ))}
+
+        <li>
+          <a
+            href={`${process.env.DOMAIN}/cv/CV_Lukasz_Grzegorzewski_${localeLanguage}.pdf`}
+            target="_blank"
+            rel="noreferrer"
+            className="text-secondary"
+          >
+            CV
+          </a>
+        </li>
+
+        <li>
+          <select
+            value={localeLanguage}
+            onChange={(e) => changLanguage(e.target.value as LocalLanguage)}
+            className="bg-transparent border-none text-secondary-dark focus:outline-none "
+          >
+            <option value="fr">
+              <span className="text-3xl">🇫🇷</span>
+            </option>
+            <option value="en">
+              <span className="text-3xl">🇬🇧</span>
+            </option>
+            <option value="pl">
+              <span className="text-3xl">🇵🇱</span>
+            </option>
+          </select>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavbarMobile;
